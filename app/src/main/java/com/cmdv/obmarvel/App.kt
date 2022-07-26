9e2c6b28f34208a3425acf638f19dd8078f58cd9
dvmatias <matias.delv.domgmail.com>
2022-07-26 11:41:49 -0300

    Code clean up.

--- app/src/main/java/com/cmdv/obmarvel/App.kt
+++ app/src/main/java/com/cmdv/obmarvel/App.kt
@@ -5,12 +5,18 @@ import com.cmdv.obmarvel.koin.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+/**
+ * Application class. Main app entry point.
+ */
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin()
     }
 
+    /**
+     * Init Koin in this application as a standalone instance.
+     */
     private fun initKoin() {
         startKoin {
             androidContext(this@App)
