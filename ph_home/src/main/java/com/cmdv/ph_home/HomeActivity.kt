b0f84c612e8d0cbdab32cae0fd129ebee414411a
dvmatias <matias.delv.domgmail.com>
2022-07-27 12:53:26 -0300

    Feature Home:  - Added CharactersFragment.kt and FavoritesFragment.kt  - Added navigation actions and destinations.

--- ph_home/src/main/java/com/cmdv/ph_home/HomeActivity.kt
+++ ph_home/src/main/java/com/cmdv/ph_home/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.cmdv.ph_home
 
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.cmdv.core.base.BaseActivity
 import com.cmdv.ph_home.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseActivity<HomeActivity, ActivityHomeBinding>(R.layout.activity_home) {
+    private lateinit var navController: NavController
+
     override fun initView() {
-        TODO("Not yet implemented")
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.content.bottomNav, navController)
+
+        // TODO setup toolbar
     }
 
     override fun observe() {
---
+++ ph_home/src/main/java/com/cmdv/ph_home/ui/fragments/CharactersFragment.kt
@@ -0,0 +1,25 @@
+package com.cmdv.ph_home.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cmdv.ph_home.R
+
+/**
+ * This Fragment is used to display the list of characters.
+ */
+class CharactersFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_characters, container, false)
+    }
+}
