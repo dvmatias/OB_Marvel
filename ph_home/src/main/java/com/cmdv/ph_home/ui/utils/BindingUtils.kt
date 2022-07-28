84e8e12477a0f1e278974fdd288c79321c42811f
dvmatias <matias.delv.domgmail.com>
2022-07-28 13:46:46 -0300

    Feature Home - Favorites:  - Added logic to display favorites, loading and empty state.

--- ph_home/src/main/java/com/cmdv/ph_home/ui/utils/BindingUtils.kt
+++ ph_home/src/main/java/com/cmdv/ph_home/ui/utils/BindingUtils.kt
@@ -1,15 +1,23 @@
 package com.cmdv.ph_home.ui.utils
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.cmdv.common.R
 import com.cmdv.common.extensions.secureUrl
 import com.cmdv.domain.models.CharacterModel
-import com.cmdv.domain.utils.ResponseWrapper
+import com.cmdv.domain.utils.ResponseWrapper.Status
+import com.cmdv.domain.utils.ResponseWrapper.Status.*
 import com.cmdv.ph_home.ui.adapters.CharacterAdapter
+import com.cmdv.ph_home.ui.adapters.IndexFavoriteCharacterAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @BindingAdapter("loadCharacters")
 fun RecyclerView.loadCharacters(
@@ -27,10 +35,10 @@ fun RecyclerView.loadCharacters(
 @BindingAdapter(value = ["viewModelStatus", "adapter"], requireAll = false)
 fun loadingVisibility(
     view: FrameLayout?,
-    viewModelStatus: ResponseWrapper.Status,
+    viewModelStatus: Status,
     adapter: CharacterAdapter
 ) {
-    if (viewModelStatus == ResponseWrapper.Status.LOADING) {
+    if (viewModelStatus == LOADING) {
         view?.visibility = if (adapter.isEmpty()) View.VISIBLE else View.GONE
         adapter.isLoading = !adapter.isEmpty()
     } else {
@@ -44,9 +52,63 @@ fun loadCharacterImage(imageView: ImageView, imageUrl: String?) {
     imageUrl?.let {
         Glide.with(imageView.context)
             .load(it.secureUrl())
-            .placeholder(com.cmdv.common.R.drawable.img_mock_character)
+            .placeholder(R.drawable.img_mock_character)
             .dontAnimate()
             .centerCrop()
             .into(imageView)
+    }
+}
+
+@BindingAdapter("loadFavoriteCharacters")
+fun RecyclerView.loadFavoriteCharacters(
+    data: MutableList<CharacterModel>?
+) {
+    adapter.run {
+        if (this is IndexFavoriteCharacterAdapter && data != null) {
+            if (data.isEmpty()) clear() else setCharacters(data)
+        }
+    }
+}
+
+@BindingAdapter("favoritesLoadingVisibility")
+fun favoritesLoadingVisibility(
+    view: FrameLayout?,
+    viewModelStatus: Status
+) {
+    view?.visibility = when (viewModelStatus) {
+        LOADING -> View.VISIBLE
+        else -> View.GONE
+    }
+}
+
+@BindingAdapter(value = ["viewModelStatus", "favoriteCharacters"], requireAll = true)
+fun favoritesEmptyStateVisibility(
+    view: ImageView?,
+    viewModelStatus: Status,
+    favoriteCharacters: List<CharacterModel>?
+) {
+    when (viewModelStatus) {
+        LOADING,
+        ERROR -> view?.visibility = View.GONE
+        READY -> {
+            if (favoriteCharacters?.isEmpty() == true) {
+                view?.run {
+                    CoroutineScope(Dispatchers.Main.immediate).launch {
+                        setImageDrawable(null)
+                        visibility = View.VISIBLE
+                        delay(150)
+                        setImageDrawable(
+                            androidx.core.content.ContextCompat.getDrawable(
+                                view.context,
+                                R.drawable.animated_empty_state_spider_man
+                            )
+                        )
+                        (drawable as AnimatedVectorDrawable).start()
+                    }
+                }
+            } else {
+                view?.visibility = View.GONE
+            }
+        }
     }
 }
