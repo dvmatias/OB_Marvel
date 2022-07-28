be4a31ef28c3598b7668e25e6f95299f149ab250
dvmatias <matias.delv.domgmail.com>
2022-07-28 16:45:22 -0300

    Feature Character Details:  - Created CharacterDetailsActivity.kt activity.  - Added navigation point in NavigatorImpl.kt navigation class.

---
+++ ph_character_details/src/main/java/com/cmdv/ph_character_details/CharacterDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.cmdv.ph_character_details
+
+import com.cmdv.core.base.BaseActivity
+import com.cmdv.ph_character_details.databinding.ActivityCharacterDetailsBinding
+
+class CharacterDetailsActivity :
+    BaseActivity<CharacterDetailsActivity, ActivityCharacterDetailsBinding>(R.layout.activity_character_details) {
+    override fun initView() {
+        // TODO("Not yet implemented")
+    }
+
+    override fun observe() {
+        // TODO("Not yet implemented")
+    }
+}
