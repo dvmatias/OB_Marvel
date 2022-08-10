9ec96bee91e6b4c8619e582202dff5f460d5966f
dvmatias <matias.delv.domgmail.com>
2022-08-10 14:39:49 -0300

    Feature Search:  - Added ph_search_character module  - Added SearchCharacterActivity.kt activity class.  - Settled gradle dependencies.  - AndroidManifest updated.

---
+++ ph_search_character/src/main/java/com/cmdv/ph_search_character/ui/SearchCharacterActivity.kt
@@ -0,0 +1,12 @@
+package com.cmdv.ph_search_character.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cmdv.ph_search_character.R
+
+class SearchCharacterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_character)
+    }
+}
