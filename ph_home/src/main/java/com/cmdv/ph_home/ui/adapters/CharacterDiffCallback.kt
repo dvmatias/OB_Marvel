75a1fafb61f890574dce4e00d33a522471f9e211
dvmatias <matias.delv.domgmail.com>
2022-08-09 21:54:32 -0300

    Code clean up.

---
+++ ph_home/src/main/java/com/cmdv/ph_home/ui/adapters/CharacterDiffCallback.kt
@@ -0,0 +1,25 @@
+package com.cmdv.ph_home.ui.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.cmdv.domain.models.CharacterModel
+
+class CharacterDiffCallback(
+    private val oldList: List<CharacterModel>,
+    private val mewList: List<CharacterModel>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = mewList.size
+
+    override fun areItemsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        return (oldList[oldPosition].id == mewList[newPosition].id)
+    }
+
+    override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        val oldCharacter = oldList[oldPosition]
+        val newCharacter = mewList[newPosition]
+
+        return oldCharacter.isFavourite == newCharacter.isFavourite
+    }
+}
